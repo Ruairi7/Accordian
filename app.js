83ee26dc25bc99b901a5855f64a50b227755f364
Ruairi <ruairilane@gmail.com>
2021-03-22 08:20:01 +0000

    Happy enough

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('hello');
+
+function myFunction(e) {
+    var x = e.parentElement;
+    var y = x.nextElementSibling;
+    
+      /*if(y.clientHeight) {
+          y.style.height = "0";
+          y.style.overflow = "hidden";
+          y.style.zIndex = "1";
+          
+      }else{
+          y.style.height = "2rem";
+          y.style.overflow = "auto";
+          y.style.zIndex = '9';
+          
+      }*/
+
+      
+
+      if (y.style.maxHeight) {
+        y.style.maxHeight = null;
+        e.querySelector('img').style.transform = "scale(1.5)";
+      } else {
+        y.style.maxHeight = y.scrollHeight + "px";
+        e.querySelector('img').style.transform = "scaleX(-1.5) scaleY(-1.5)";
+      }
+    
+
+  }
+
+  
+
+
